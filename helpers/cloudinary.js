0f4587d310e62ba4847e2d42cae8c712346c9978
luisve9 <luissamdoval.9@gmail.com>
2021-08-23 17:33:07 -0500

    add utils

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: "lab-profileapp",
+    folder: "artify-alpha",
     use_filename: true,
     allowedFormats:["jpg","png","jpeg","pdf"]
   },
