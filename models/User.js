f4423f4b27c42ffc087f761e90350e0ed76d1154
luisve9 <luissamdoval.9@gmail.com>
2021-08-23 14:55:20 -0500

    Model user modif

--- models/User.js
+++ models/User.js
@@ -3,15 +3,14 @@ const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
   {
-    
+    email: {
+      type:String,
+      required: [true, "Debes mandar un email"],
+    },
     username: {
       type: String,
       required: [true, "Debes mandar un Nombre de usuario"],
     },
-    campus: {
-        type: String,
-        enum: ["Madrid", "Barcelona", "Miami", "Paris", "Berlin", "Amsterdam", "México", "Sao Paulo", "Lisbon"]
-      },
     password: {
         type: String,
         required: [true, "Debes agregar que un contraseña"],
@@ -19,9 +18,9 @@ const userSchema = new Schema(
     image:{
         type:String,
     },
-    course: {
+    role: {
       type:String,
-      enum: ["Web Dev", "UX/UI", "Data Analytics"]
+      enum:["Admin","User","Creator"]
     }
   },
   { timestamps: true }
